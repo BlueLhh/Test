c95a00cc2263540402aa93623a4c222ad3461b93
Lan <lanhonghui100@gmail>
2017-12-01 18:16:30 +0800

    新的测试

--- src/com/svntest/test/Test.java
+++ src/com/svntest/test/Test.java
@@ -4,5 +4,10 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("This is a test!");
 		System.out.println("这是一个测试");
+		System.out.println("这是一个测试");
+		System.out.println("这是一个测试");
+		System.out.println("这是一个测试");
+		System.out.println("这是一个测试");
+		System.out.println("这是一个测试");
 	}
 }
